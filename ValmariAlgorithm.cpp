881805b991bc68bc819923f0ff355ab4b71cd9d7
CovarianceMomentum <covariancemomentum@gmail.com>
2020-05-06 01:25:54 +0300

    Added file for Valmari Algorithm

--- ValmariAlgorithm.cpp
+++ ValmariAlgorithm.cpp
@@ -1,4 +1,9 @@
+/* Code below reproduced from "Fast brief practical DFA minimization" by Antti Valmari (2011) */
+
 //
 // Created by covariance on 06.05.2020.
 //
 
+#include "DFA.h"
+
+/* code to be included */
